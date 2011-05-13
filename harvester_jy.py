e6f74ee34e7a5ac1c2d01ac064b5d3a3c92198ab
Rafael Novello <rafa.reis.novello@gmail.com>
2011-05-13 17:34:37 -0300

    Adjusted to use json file to save providers resumptionToken

--- harvester_jy.py
+++ harvester_jy.py
@@ -5,8 +5,7 @@ from sys import path
 from glob import glob
 from os.path import exists
 import settings
-import json
-
+from com.xhaus.jyson import JysonCodec as json
 
 libDir='/home/rafael/aplicacoes/oai-pmh/java/lib/harvester2-0.1.12'
 
@@ -16,38 +15,39 @@ for jar in glob(libDir+'/*.jar'):
 from ORG.oclc.oai.harvester2.verb import GetRecord
 from ORG.oclc.oai.harvester2.verb import ListRecords
 
-def listRecords(url, from_date='', until_date='', setSpec='', prefix=''):
+def listRecords(url, from_date='', until_date='', setSpec='', prefix='', token=''):
     content = ''
     loop = 0
-    res = loadResumptionToken('scielo')
-    if res == None:
+    if token == None:
         records = ListRecords(url, from_date, until_date, setSpec, prefix)
     else:
-        records = ListRecords(url, res)
+        records = ListRecords(url, token)
     while records:
         #import pdb; pdb.set_trace()
         loop += 1
         content += records.toString() + "\n"
-        errors = records.getErrors()
-        res = records.getResumptionToken()
+        errors = records.getErrors().toString()
+        token = records.getResumptionToken()
         if errors != None or errors.length > 0:
-            writeErrorLog(res + "\n")            
-        if res == None or len(res) == 0:
+            #writeErrorLog(errors + "\n")
+            pass            
+        if token == None or len(token) == 0:
             content=''
             break
-        elif (res != None or len(res) != 0) and loop >= LIMIT_TO_CALLS:
-            saveResumptionToken('scielo', res)
+        elif (token != None or len(token) != 0) and loop >= settings.LIMIT_TO_CALLS:
+            saveResumptionToken('scielo', token)
             break
         else:
-            records = ListRecords(url, res)
+            records = ListRecords(url, token)
     return content
 
 
-def writeXMLFile(content):
-    if exists('oai.xml'):
-        xmlFile = open('oai.xml', 'a')
+def writeXMLFile(provider, content):
+    fileName = provider+'.xml'
+    if exists(fileName):
+        xmlFile = open(fileName, 'a')
     else:
-        xmlFile = open('oai.xml', 'w')
+        xmlFile = open(fileName, 'w')
     xmlFile.write(content.encode('utf-8') + "\n")
     xmlFile.close()
 
@@ -58,18 +58,44 @@ def writeErrorLog(content):
     
 def saveResumptionToken(provider, token):
     tf = open('token.json', 'r')
-    tokenDict = json.load(tf)
+    tokenDict = json.loads(tf.read())
     tf.close()
     
     tokenDict[provider]['token'] = token
     
-    with open('token.json', 'w') as tf:
-        json.dump(tokenDict, tf, indent=2)
+    tf = open('token.json', 'w')
+    tf.write(json.dumps(tokenDict))
+    tf.close()
 
 def loadResumptionToken(provider):
-    with open('token.json', 'r') as tf:
-        res = json.load(tf)
+    tf = open('token.json', 'r')
+    res = json.loads(tf.read())
+    tf.close()
     
     return res[provider]['token']
 
-
+def doHarvest():
+    for provider in settings.PROVIDERS:
+        name = provider[0]
+        url = provider[1]
+        token = loadResumptionToken(name)
+        content = listRecords(url, 'oai_dc')
+        writeXMLFile(name, content)
+        print "Harvest done for %s provider" % name
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
