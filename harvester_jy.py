3836b5c35271008eedf12fc411fe0fcc7a479df3
Rafael Novello <rafa.reis.novello@gmail.com>
2011-05-18 20:36:14 -0300

    start date control. Not so good yet

--- harvester_jy.py
+++ harvester_jy.py
@@ -6,16 +6,19 @@ from glob import glob
 from os.path import exists
 import settings
 from com.xhaus.jyson import JysonCodec as json
+import xml.etree.ElementTree as etree
+
 
 libDir='/home/rafael/aplicacoes/oai-pmh/java/lib/harvester2-0.1.12'
 
 for jar in glob(libDir+'/*.jar'):
     path.append(jar)
 
-from ORG.oclc.oai.harvester2.verb import GetRecord
+from ORG.oclc.oai.harvester2.verb import Identify
 from ORG.oclc.oai.harvester2.verb import ListRecords
 
-def listRecords(url, provider, from_date='', until_date='', setSpec='', prefix='', token=''):
+def listRecords(url, provider, from_date='', until_date='', setSpec='', 
+                prefix='', token=None):
     content = ''
     loop = 0
     if token == None:
@@ -34,11 +37,16 @@ def listRecords(url, provider, from_date='', until_date='', setSpec='', prefix='
         if token == None or len(token) == 0:
             content=''
             break
-        elif (token != None or len(token) != 0) and loop >= settings.LIMIT_TO_CALLS:
+        elif (token != None or len(token) != 0): #and loop >= settings.LIMIT_TO_CALLS:
             saveResumptionToken(provider, token)
+            saveUntilDate()
             break
         else:
             records = ListRecords(url, token)
+    return content
+    
+def identifyProvider(url):
+    content = Identify(url)
     return content
 
 
@@ -73,13 +81,36 @@ def loadResumptionToken(provider):
     tf.close()
     
     return res[provider]['token']
+    
+def getFromDate(provider):
+    tf = open('token.json', 'r')
+    res = json.loads(tf.read())
+    tf.close()
+    
+    providerName = provider[0]
+    lastDate = res[providerName]['date']
+    
+    if lastDate:
+        fromdate = lastDate
+    else:
+        fromDate = getInitDate(provider)
+    return fromDate
+    
+def getInitDate(provider):
+    url = provider[1]
+    content = identifyProvider(url)
+    root = etree.XML(content.toString())
+    #FIXME: pegar a data do XML de um jeito correto
+    return root[2][4].text
+    
 
 def doHarvest():
     for provider in settings.PROVIDERS:
         name = provider[0]
         url = provider[1]
+        date = getFromDate(provider)
         token = loadResumptionToken(name)
-        content = listRecords(url, name, 'oai_dc')
+        content = listRecords(url, name, date, prefix='oai_dc')
         writeXMLFile(name, content)
         print "Harvest done for %s provider" % name
     
