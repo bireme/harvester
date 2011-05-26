21e7fcddcbdfcaab23e3a0e40b43e5aa94384dfb
Rafael Novello <rafa.reis.novello@gmail.com>
2011-05-26 15:17:16 -0300

    added save and load functions to manage harvest last date

--- harvester.py
+++ harvester.py
@@ -2,8 +2,10 @@ from customclient import CustomClient
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 from oaipmh.server import oai_dc_writer
 from os.path import exists
+from datetime import datetime
 from os import makedirs
 import settings
+import json
 
 BASE_PATH = '/home/rafael/aplicacoes/oai-pmh/script/tmp/'
 
@@ -12,7 +14,7 @@ registry = MetadataRegistry()
 registry.registerReader('oai_dc', oai_dc_reader)
 registry.registerWriter('oai_dc', oai_dc_writer)
 
-def getClientsIterator():
+def getProvidersIterator():
     for provider in settings.PROVIDERS:
         providerDir = BASE_PATH+provider[0]
         if not exists(providerDir):
@@ -21,9 +23,33 @@ def getClientsIterator():
         
 
 def doHarvest():
-    for client in getClientsIterator():
-        iterator = client.listRecords(metadataPrefix='oai_dc')
-        for header, metadata, about in iterator:
-            client.save()
+    for provider in getProvidersIterator():
+        iterator = provider.listRecords(metadataPrefix='oai_dc')
+        for rec in iterator:
+            provider.save()
 
+def getInitDate(provider):
+    about = provider.identify()
+    return about.earliestDatestamp()
+    
+def saveLastDate(provider, date):
+    repoName = provider[0]
+    entry = {}
+    entry[repoName] = {}
+    entry[repoName]['year'] = date.year
+    entry[repoName]['month'] = date.month
+    entry[repoName]['day'] = date.day
+    
+    with open('teste.json', mode='a+') as f:
+        json.dump(entry, f, indent=2)
 
+def loadLastDate(provider):
+    with open('teste.json', mode='r') as f:
+        entry = json.load(f)
+        
+    repoName = provider[0]
+    lastDate = datetime(entry[repoName]['year'],
+                        entry[repoName]['month'],
+                        entry[repoName]['day']
+                        )
+    return lastDate
