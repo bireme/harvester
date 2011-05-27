693394cd9d14451a023a9cd2b1a098b7be2313a8
Rafael Novello <rafa.reis.novello@gmail.com>
2011-05-27 18:00:21 -0300

    Added argparse to control prompt calls

--- harvester.py
+++ harvester.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 from customclient import CustomClient
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 from oaipmh.server import oai_dc_writer
 from os.path import exists
 from datetime import datetime
 from os import makedirs
+import argparse
 import settings
 import json
 import sys
@@ -26,12 +31,19 @@ class Harvester(object):
             yield CustomClient(provider[1], providerDir, self._registry)
             
 
-    def doHarvest():
-        for provider in getProvidersIterator():
-            date = getFromDate(provider)
+    def doMultHarvest(self, providers):
+        for provider in providers:
+            date = self.getFromDate(provider)
             iterator = provider.listRecords(metadataPrefix='oai_dc', from_=date)
             for rec in iterator:
                 provider.save()
+                
+    def doHarvest(self, provider, metadata, from_, until):
+        import pdb; pdb.set_trace()
+        iterator = provider.listRecords(metadataPrefix=metadata, 
+                                        from_=from_, until=until)
+        for rec in iterator:
+            provider.save()
 
     def getFromDate(provider):
         pName = provider._name
@@ -80,6 +92,68 @@ if __name__ == '__main__':
     
     harv = Harvester(registry)
     
-    if len(sys.argv) == 1:
-        #harv.doHarvest()
-        pass
+    parser = argparse.ArgumentParser(
+        description='Make harvest from any repositories and save the metadata in file system')
+        
+    parser.add_argument(
+        '-u', '--url', type=str, default=None,
+        help='an alternative URL for harvesting. Ignore settings file')   
+    
+    parser.add_argument(
+        '-f', '--from_', type=str, default=None,
+        help='an initial date to harvest')
+        
+    parser.add_argument(
+        '-t', '--to', type=str, default=None,
+        help='a final date to harvest')
+        
+    parser.add_argument(
+        '-m', '--metadata', type=str, default='oai_dc',
+        help='specify the metadataPrefix for OAI protocol')
+        
+    parser.add_argument(
+        '-g', '--go', action='store_true', 
+        help='makes harvest from a default providers list')
+        
+    args = parser.parse_args()
+    if args.go:
+        providers = harv.getProvidersIterator()
+        harv.doMultHarvest(providers)
+    else:
+        from_=datetime.strptime(args.from_, '%Y-%m-%d')
+        until=datetime.strptime(args.to, '%Y-%m-%d')
+        provider = CustomClient(args.url, BASE_PATH, harv._registry)
+        harv.doHarvest(provider, args.metadata, from_=from_, until=until)
+        
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
