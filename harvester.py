01ba7e0df553795e33e8968539853e8c3313efb0
Rafael Novello <rafa.reis.novello@gmail.com>
2011-08-05 17:54:18 -0300

    Enable oai_dc

--- harvester.py
+++ harvester.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from oaipmh.metadata import MetadataRegistry, oai_dc_reader, MetadataReader
+from oaipmh.metadata import MetadataRegistry, oai_dc_reader
+from metadatareaders import qdc_reader
 from customclient import CustomClient
 from oaipmh.server import oai_dc_writer
 from os.path import exists
@@ -80,43 +81,8 @@ class Harvester(object):
     
 if __name__ == '__main__' :
     
-    qdc_reader = MetadataReader(
-        fields={
-        'author':                ('textList', 'dcterms:author/text()'),
-        'accessioned':           ('textList', 'dcterms:accessioned/text()'),
-        'available':             ('textList', 'dcterms:available/text()'),
-        'issued':                ('textList', 'dcterms:issued/text()'),
-        'bibliographicCitation': ('textList', 'dcterms:issued/text()'),
-        'issn':                  ('textList', 'dcterms:issn/text()'),
-        'identifier':            ('textList', 'dc:identifier/text()'),
-        'description':           ('textList', 'dc:description/text()'),
-        'abstract':              ('textList', 'dcterms:abstract/text()'),
-        'sponsorship':           ('textList', 'dcterms:sponsorship/text()'),
-        'language':              ('textList', 'dc:language/text()'),
-        'publisher':             ('textList', 'dc:publisher/text()'),
-        'ispartofseries':        ('textList', 'dcterms:ispartofseries/text()'),
-        'subject':               ('textList', 'dc:subject/text()'),
-        'title':                 ('textList', 'dc:title/text()'),
-        'alternative':           ('textList', 'dcterms:alternative/text()'),
-        'type':                  ('textList', 'dc:type/text()'),
-        'format':                ('textList', 'dc:format/text()'),
-        'source':                ('textList', 'dc:source/text()'),
-        'relation':              ('textList', 'dc:relation/text()'),
-        'coverage':              ('textList', 'dc:coverage/text()'),
-        'rights':                ('textList', 'dc:rights/text()')
-        },
-        namespaces={
-        'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-        'dc' : 'http://purl.org/dc/elements/1.1/',
-        'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-        'schemaLocation': 'http://purl.org/dc/terms/ http://dublincore.org/schemas/xmls/qdc/2006/01/06/dcterms.xsd http://purl.org/dc/elements/1.1/ http://dublincore.org/schemas/xmls/qdc/2006/01/06/dc.xsd',
-        'lang': 'en_US',
-        'dcterms': 'http://purl.org/dc/terms/'}
-        )
-    
-    
     registry = MetadataRegistry()
-    #registry.registerReader('oai_dc', oai_dc_reader)
+    registry.registerReader('oai_dc', oai_dc_reader)
     registry.registerReader('qdc', qdc_reader)
     registry.registerWriter('oai_dc', oai_dc_writer)
     
